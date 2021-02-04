cb29f99545f389eecbb870906b6145fcf71fa35f
Ian Cossentino <icossentino@gmail.com>
2021-02-04 16:41:34 -0500

    added sign in method to sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,9 +13,30 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
+  def sign_in_user
+    user = User.find_by(email: params[:user][:email])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to '/dashboard'
+    else
+      flash[:alert] = 'Error logging in, please try again'
+      render :login
+    end
+  end
+
+
   ##Create user via oauth and login via oauth in same method here
   def oauth
   end
+
+
+  private
+
+  def user_params_for_sessions
+    params.require(:user).permit(:email, :password, :password_confirmation, :income, :first_name, :last_name)
+  end
+
+
 
 
 end
