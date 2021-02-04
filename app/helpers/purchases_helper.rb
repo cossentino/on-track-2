7fa0dea4d7964a349b103b77196cf29bc0ec156e
Ian Cossentino <icossentino@gmail.com>
2021-02-04 17:43:30 -0500

    creating scope method for current user's categories

--- app/helpers/purchases_helper.rb
+++ app/helpers/purchases_helper.rb
@@ -1,2 +1,8 @@
 module PurchasesHelper
+
+  def my_categories
+    Category.where(user_id: signed_in_user.id).uniq
+  end
+
+
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,18 @@
 class Category < ApplicationRecord
   has_many :purchases
   has_many :users, through: :purchases
+
+
+  def my_categories
+
+SELECT DISTINCT name
+FROM categories
+INNER JOIN purchases
+ON categories.id = purchases.category_id
+WHERE purchases.user_id = 1
+
+
+
+
+
 end
