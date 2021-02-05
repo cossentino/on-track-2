acf0bcb534a851d01a65d5f8d05ac145da7e596a
Ian Cossentino <icossentino@gmail.com>
2021-02-05 10:26:07 -0500

    refactoring new purchase page

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -3,12 +3,23 @@ class PurchasesController < ApplicationController
   before_action :user_verification
 
   def new
-    user_verification
     @purchase = Purchase.new
+    @user = current_user
   end
 
   def index
     @purchases = Purchase.where(user_id: current_user.id )
+  end
+
+  def create
+
+    purchase = Purchase.new(purchase_params)
+    if purchase.save
+      redirect_to '/dashboard'
+    else
+      flash[:alert] = "Purchase couldn't save, please try again"
+      render :new
+    end
   end
 
 
@@ -16,6 +27,10 @@ class PurchasesController < ApplicationController
 
   def user_verification
     redirect_to root_path unless session[:user_id] == params[:user_id].to_i
+  end
+
+  def purchase_params
+    params.require(:purchase).permit(:location, :date, :note, :total, :category_id)
   end
 
   
