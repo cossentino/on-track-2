a8f0cbf5954b1a260946ee8f9f295dca60e15106
Ian Cossentino <icossentino@gmail.com>
2021-02-04 21:37:59 -0500

    added date filter for purchases

--- app/helpers/purchases_helper.rb
+++ app/helpers/purchases_helper.rb
@@ -1,8 +1,3 @@
 module PurchasesHelper
 
-  def my_categories
-    Category.where(user_id: signed_in_user.id).uniq
-  end
-
-
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,24 @@
 class Purchase < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  def self.current_month
+    Time.now.strftime('%m')
+  end
+
+  def self.current_year
+    Time.now.strftime('%Y')
+  end
+
+  def self.last_day_of_month
+    Date.civil(Time.now.year, Time.now.month, -1).day
+  end
+
+  def self.current_month_purchases
+    Purchase.where('purchases.date BETWEEN ? AND ?', "#{current_year}-#{current_month}-01", "#{current_year}-#{current_month}-#{last_day_of_month}")
+  end
 end
+
+
+
+# "#{Purchase.current_year}-#{Purchase.current_month}-1", "#{current_year}-#{current_month}-#{last_day_of_month}")
