d62b061d3f9fe24b88f11064cb5727b6c1bd1d96
Ian Cossentino <icossentino@gmail.com>
2021-02-16 18:13:11 -0500

    refactored income percentage into helper method

--- app/helpers/purchases_helper.rb
+++ app/helpers/purchases_helper.rb
@@ -4,4 +4,8 @@ module PurchasesHelper
     Purchase.current_month_purchases(signed_in_user).pluck(:total).sum
   end
 
+  def percent_of_income
+    ((current_month_total / signed_in_user.income) * 100).round(1)
+  end
+
 end
